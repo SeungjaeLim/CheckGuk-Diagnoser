dccdfb57bf8a2222f5cc8ee5b03b7e6bd11f6895
Seungjae <seungjaelim@kaist.ac.kr>
2023-05-09 18:03:24 +0900

    FEAT: Connect to backend server

--- checkuk/src/App.js
+++ checkuk/src/App.js
@@ -1,8 +1,20 @@
 import React, { useState } from "react";
-import { Configuration, OpenAIApi } from "openai";
-import { Button, Card, CardActions, CardContent, Grid, Tab, Tabs, Typography, createMuiTheme, ThemeProvider, TextField, IconButton } from "@material-ui/core";
+import API_ENDPOINT from './config';
+import {
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  Grid,
+  Tab,
+  Tabs,
+  Typography,
+  createMuiTheme,
+  ThemeProvider,
+  TextField,
+  IconButton,
+} from "@material-ui/core";
 import { LocalHospital } from "@material-ui/icons";
-
 import "./App.css";
 import poster from "./assets/poster.png";
 
@@ -17,6 +29,28 @@ const theme = createMuiTheme({
   },
 });
 
+function parseText(text) {
+  const parsedText = {};
+
+  const prescriptionMatch = text.match(/처방: (.*?)\n추천하는 책:/);
+  if (prescriptionMatch) {
+    parsedText.prescription = prescriptionMatch[1];
+  }
+
+  const recommendedBookMatch = text.match(/추천하는 책: (.*?)(?:\n\n|\n)처방에 대한 근거와 설명:/);
+  if (recommendedBookMatch) {
+    parsedText.recommendedBook = recommendedBookMatch[1];
+  }
+
+  const explanationMatch = text.match(/처방에 대한 근거와 설명:\s*(.*)/
+  );
+  if (explanationMatch) {
+    parsedText.explanation = explanationMatch[1];
+  }
+
+  return parsedText;
+}
+
 function App() {
   const [input1, setInput1] = useState("");
   const [input2, setInput2] = useState("");
@@ -24,34 +58,26 @@ function App() {
   const [output, setOutput] = useState("");
   const [selectedTab, setSelectedTab] = useState(0);
 
-  const openaiApiKey = "my key"; // Replace with your OpenAI API Key
-  const configuration = new Configuration({ apiKey: openaiApiKey });
-  const openai = new OpenAIApi(configuration);
-
-  const handleClick = async () => {
-    try {
-      const prompt = `안: ${input1}\n옆: ${input2}\n밖: ${input3}\nOutput:`;
-
-      const response = await openai.createCompletion({
-        model: "text-davinci-003",
-        prompt,
-        temperature: 0.5,
-        max_tokens: 50,
-        top_p: 1,
-        frequency_penalty: 0.0,
-        presence_penalty: 0.0,
-        stop: ["\n"],
-      });
-
-      if (response.choices && response.choices.length > 0) {
-        setOutput(response.choices[0].text.trim());
-      } else {
-        setOutput("No response generated.");
-      }
-    } catch (error) {
-      console.error("Error generating output:", error);
-    }
-  };
+  const handleClick = (e) => {
+    const message = `${input1}. ${input2}. ${input3}`;
+    console.log(JSON.stringify({prompt}))
+    e.preventDefault()
+    fetch(`${API_ENDPOINT}/`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({message})
+    })
+    .then((res) => res.json())
+    .then((data) => {
+      const parsedResponse = parseText(data.message.replace(/\\n/g, '\n'));
+      console.log(data.message.replace(/\\n/g, '\n'))
+      console.log(parsedResponse)
+      setOutput(parsedResponse);
+    });
+  }
+  
 
   const handleTabChange = (event, newValue) => {
     setSelectedTab(newValue);
@@ -68,132 +94,197 @@ function App() {
             <Tab label="Contact" />
           </Tabs>
         </header>
+
         <main>
         {selectedTab === 0 && (
   <Grid container spacing={3} direction="column">
     <Grid item xs={12}>
       <Card>
         <CardContent>
-          <Typography variant="h5" gutterBottom style={{textAlign: "center"}}>
-            <img src="https://cdn.pixabay.com/photo/2016/10/06/19/02/book-1719737_960_720.png" style={{height: "1em"}} alt="Rx symbol"/>  독서 처방  <img src="https://cdn.pixabay.com/photo/2016/10/06/19/02/book-1719737_960_720.png" style={{height: "1em"}} alt="Rx symbol"/>
+          <Typography variant="h5" gutterBottom style={{ textAlign: "center" }}>
+            <img
+              src="https://cdn.pixabay.com/photo/2016/10/06/19/02/book-1719737_960_720.png"
+              style={{ height: "1em" }}
+              alt="Rx symbol"
+            />{" "}
+            독서 처방{" "}
+            <img
+              src="https://cdn.pixabay.com/photo/2016/10/06/19/02/book-1719737_960_720.png"
+              style={{ height: "1em" }}
+              alt="Rx symbol"
+            />
           </Typography>
-          <Typography variant="subtitle1" gutterBottom>자신의 답변, 자신이 파밍한 답변, 자신이 구매한 답변을 적어주세요</Typography>
-          <div style={{border: "1px solid #ccc", borderRadius: "5px", padding: "20px"}}>
+
+          <Typography variant="subtitle1" gutterBottom>
+            자신의 답변, 자신이 파밍한 답변, 자신이 구매한 답변을 적어주세요
+          </Typography>
+
+          <div
+            style={{
+              border: "1px solid #ccc",
+              borderRadius: "5px",
+              padding: "20px",
+            }}
+          >
             <Grid container spacing={2}>
               <Grid item xs={12} sm={4}>
-                <Typography variant="body1" gutterBottom>독서 안</Typography>
+                <Typography variant="body1" gutterBottom>
+                  독서 안
+                </Typography>
                 <TextField
                   variant="outlined"
                   fullWidth
+                  multiline
+                  rows={4}
                   value={input1}
                   onChange={(e) => setInput1(e.target.value)}
                 />
               </Grid>
               <Grid item xs={12} sm={4}>
-                <Typography variant="body1" gutterBottom>독서 옆</Typography>
+                <Typography variant="body1" gutterBottom>
+                  독서 옆
+                </Typography>
                 <TextField
                   variant="outlined"
                   fullWidth
+                  multiline
+                  rows={4}
                   value={input2}
                   onChange={(e) => setInput2(e.target.value)}
                 />
               </Grid>
               <Grid item xs={12} sm={4}>
-                <Typography variant="body1" gutterBottom>독서 밖</Typography>
+                <Typography variant="body1" gutterBottom>
+                  독서 밖
+                </Typography>
                 <TextField
                   variant="outlined"
                   fullWidth
+                  multiline
+                  rows={4}
                   value={input3}
                   onChange={(e) => setInput3(e.target.value)}
                 />
               </Grid>
+
               <Grid item xs={12}>
                 <Grid container justify="center" alignItems="center">
                   <Grid item>
-                    <IconButton variant="contained" color="primary" onClick={handleClick}>
+                    <IconButton
+                      variant="contained"
+                      color="primary"
+                      onClick={handleClick}
+                    >
                       <LocalHospital />
                     </IconButton>
                   </Grid>
                 </Grid>
               </Grid>
               <Grid item xs={12}>
-                <Card variant="outlined">
-                  <CardContent>
-                    <Typography variant="h5" gutterBottom>처방전</Typography>
-                    <Typography variant="body1">{output}</Typography>
-                  </CardContent>
-                </Card>
-              </Grid>
+  <Card variant="outlined">
+    <CardContent>
+      <Typography variant="h5" gutterBottom>
+        처방전
+      </Typography>
+      <div style={{ marginBottom: "8px" }}>
+        <Typography variant="subtitle1" gutterBottom>
+          처방
+        </Typography>
+        <Typography variant="body1" gutterBottom>
+          {output.prescription}
+        </Typography>
+      </div>
+      <div style={{ marginTop: "16px", marginBottom: "8px" }}>
+        <Typography variant="subtitle1" gutterBottom>
+          추천도서
+        </Typography>
+        <Typography variant="body1" gutterBottom>
+          {output.recommendedBook}
+        </Typography>
+      </div>
+      <div style={{ marginTop: "16px" }}>
+        <Typography variant="body1">
+          {output.explanation}
+        </Typography>
+      </div>
+    </CardContent>
+  </Card>
+</Grid>
+
             </Grid>
-            <div style={{marginTop: "20px", textAlign: "center"}}>
-              <img src="https://cdn.pixabay.com/photo/2014/12/21/23/39/pills-575765_960_720.png" style={{height: "1em"}} alt="Rx symbol"/>
-            </div>
+            <div
+              style={{ marginTop: "20px", textAlign: "center" }}
+            >
+              <img
+                src="https://cdn.pixabay.com/photo/2014/12/21/23/39/pills-575765_960_720.png"
+                style={{ height: "1em" }}
+                alt="Rx symbol"
+              />
+                        </div>
           </div>
         </CardContent>
       </Card>
     </Grid>
   </Grid>
 )}
-{selectedTab === 1 && (
-            <Grid container spacing={3} direction="column">
-              <Grid item xs={12}>
-                <Card>
-                  
-                  <CardContent>
-                    <Grid container spacing={2}>
-                      <Grid item xs={12}>
-                        <img src={poster} alt="poster" style={{ width: "100%" }} />
-                      </Grid>
-                    </Grid>
-                  </CardContent>
-                </Card>
-              </Grid>
-            </Grid>
-          )}
 
-          {selectedTab === 2 && (
-  <Grid container spacing={3} direction="column">
-    <Grid item xs={12}>
-      <Card style={{ backgroundColor: "#f8f8f8" }}>
-        <CardContent>
-          <Grid container justify="center">
-            <Grid item xs={12} md={8}>
-              <Typography variant="body1" gutterBottom>
-                If you have any questions or feedback, please contact Seungjae Lim at seungjaelim@kaist.ac.kr.
-              </Typography>
-              <Typography variant="body1">
-                You can also view the source code for this project on GitHub at <a href="https://github.com/SeungjaeLim">https://github.com/SeungjaeLim</a>.
-              </Typography>
-            </Grid>
-          </Grid>
-          <Grid container justify="center" style={{ marginTop: "2em" }}>
-            <Grid item>
-              <Button
-                variant="contained"
-                color="secondary"
-                style={{
-                  borderRadius: "20px",
-                  fontWeight: "bold",
-                  textTransform: "none",
-                  boxShadow: "none",
-                  transition: "all 0.2s ease-in-out",
-                }}
-              >
-                Send Message
-              </Button>
-            </Grid>
-          </Grid>
-        </CardContent>
-      </Card>
-    </Grid>
-  </Grid>
-)}
 
-        </main>
-      </div>
-    </ThemeProvider>
-  );
+      {selectedTab === 1 && (
+        <Grid container spacing={3} direction="column">
+          <Grid item xs={12}>
+            <Card>
+              <CardContent>
+                <Grid container spacing={2}>
+                  <Grid item xs={12}>
+                    <img src={poster} alt="poster" style={{ width: "100%" }} />
+                  </Grid>
+                </Grid>
+              </CardContent>
+            </Card>
+          </Grid>
+        </Grid>
+      )}
+
+      {selectedTab === 2 && (
+        <Grid container spacing={3} direction="column">
+          <Grid item xs={12}>
+            <Card style={{ backgroundColor: "#f8f8f8" }}>
+              <CardContent>
+                <Grid container justify="center">
+                  <Grid item xs={12} md={8}>
+                    <Typography variant="body1" gutterBottom>
+                      If you have any questions or feedback, please contact Seungjae Lim at seungjaelim@kaist.ac.kr.
+                    </Typography>
+                    <Typography variant="body1">
+                      You can also view the source code for this project on GitHub at <a href="https://github.com/SeungjaeLim">https://github.com/SeungjaeLim</a>.
+                    </Typography>
+                  </Grid>
+                </Grid>
+                <Grid container justify="center" style={{ marginTop: "2em" }}>
+                  <Grid item>
+                    <Button
+                      variant="contained"
+                      color="secondary"
+                      style={{
+                        borderRadius: "20px",
+                        fontWeight: "bold",
+                        textTransform: "none",
+                        boxShadow: "none",
+                        transition: "all 0.2s ease-in-out",
+                      }}
+                    >
+                      Send Message
+                    </Button>
+                  </Grid>
+                </Grid>
+              </CardContent>
+            </Card>
+          </Grid>
+        </Grid>
+      )}
+    </main>
+  </div>
+</ThemeProvider>);
 }
 
 export default App;
-
---
+++ checkuk/src/config.js
@@ -0,0 +1,3 @@
+// config.js
+const API_ENDPOINT = 'https://fb94-121-183-192-137.ngrok-free.app'; // replace with your ngrok URL
+export default API_ENDPOINT;
