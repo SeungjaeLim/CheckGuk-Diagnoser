b1d7092c818eac2ee59ad098c4f5a3e9ef9b2172
Seungjae <seungjaelim@kaist.ac.kr>
2023-05-15 19:30:40 +0900

    CHORE: Update ngrok

--- checkuk/src/config.js
+++ checkuk/src/config.js
@@ -1,3 +1,3 @@
 // config.js
-const API_ENDPOINT = 'https://5cd3-175-211-42-164.ngrok-free.app'; // replace with your ngrok URL
+const API_ENDPOINT = 'https://8d92-175-211-42-164.ngrok-free.app'; // replace with your ngrok URL
 export default API_ENDPOINT;
